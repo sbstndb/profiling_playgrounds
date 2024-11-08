e19ab9082f92cd3693fc906a897ff4e462eb68d6
sbstndb <sebastienduboid4@gmail.com>
2024-11-08 08:50:35 +0100

    Update file

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 // are we sure there will not be auto inlining ? 
 template <typename V>
-void compute(V& a, V& b , V& c, std::size_t size){
-	for (int i = 0 ; i < size ; i++){
+[[gnu::noinline]]
+void compute_vector(V& a, V& b , V& c, std::size_t size){
+	for (std::size_t i = 0 ; i < size ; i++){
+		// std vector
 		c[i] = a[i] + b[i] ; 
 	}
 //        for (int i = 0 ; i < size ; i++){
@@ -12,28 +14,52 @@ void compute(V& a, V& b , V& c, std::size_t size){
 //        for (int i = 0 ; i < size ; i++){
 //                b[i] = c[i] + a[i] ;
 //        }	
+}
 
+[[gnu::noinline]]
+void compute_raw(float* a, float* b , float* c, std::size_t size){
+        for (unsigned int i = 0 ; i < size ; i++){
+		// raw pointer
+                c[i] = a[i] + b[i] ;
+        }
+}
+
+
+void Hello(int test){
+	std::cout << " Hello, world " << test <<  std::endl ; 
 }
 
 
 int main(){
-	std::cout << "Hello, world " <<std::endl ; 
 
+	Hello(1) ; 
 
-	std::size_t size = 1024000 ; 
+	std::size_t size = 102400000 ; 
 	int loop = 1 ; 
 
 	std::vector<float> a(size, 0.00000001) ;
 	std::vector<float> b(size, -0.00000001) ; 
 	std::vector<float> c(size, -0.00000001) ; 
 
-	// compute
-	for (int i = 0 ; i < loop ; i++){
-		compute(a, b, c, size) ; 
-	}
+	float *d_a, *d_b, *d_c;
+	d_a = (float*)malloc(size * sizeof(float)); 
+        d_b = (float*)malloc(size * sizeof(float));
+        d_c = (float*)malloc(size * sizeof(float));
+	
 
-	// avpid code deletion
+
+	// compute
+	compute_vector(a, b, c, size) ; 
+
+	compute_raw(d_a, d_b, d_c, size);
+
+	// avoid code deletion
 	std::cout << " Last value is : " << c.back() << std::endl ;
+
+
+	free(d_a) ;
+	free(d_b) ; 
+	free(d_c) ; 
 
 	return 0 ; 
 }
